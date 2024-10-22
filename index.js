a7abc4b2885d8acf44f204a85c0ed6bc5228264c
Aditya Alshi <alshiaditya55@gmail.com>
2024-10-22 10:42:53 +0530

    vercel modified

--- index.js
+++ index.js
@@ -1,26 +1,30 @@
 const express = require('express');
 const app = express();
 const cors = require('cors')
-const { emailList, emailBodies } = require('./data')
+// const { emailList, emailBodies } = require('./data')
 
 app.use(cors());
 
 app.get('/', (req, res) => {
-    const page = parseInt(req.query.page);
-    const id = parseInt(req.query.id)
-    if(id) {
-        const emailBody = emailBodies.find(eb => eb.id === id)
-        res.status(200).json(emailBody)
-    }
-    if(page === 1) {
-        res.status(200).json({
-            list: emailList.slice(0, 10)
-        })
-    } else if(page === 2) {
-        res.status(200).json({
-            list: emailList.slice(10)
-        })
-    }
+    // const page = parseInt(req.query.page);
+    // const id = parseInt(req.query.id)
+    // if(id) {
+    //     const emailBody = emailBodies.find(eb => eb.id === id)
+    //     res.status(200).json(emailBody)
+    // }
+    // if(page === 1) {
+    //     res.status(200).json({
+    //         list: emailList.slice(0, 10)
+    //     })
+    // } else if(page === 2) {
+    //     res.status(200).json({
+    //         list: emailList.slice(10)
+    //     })
+    // }
+
+    res.json({
+        messgae: "test the joker"
+    })
 
 })
 
